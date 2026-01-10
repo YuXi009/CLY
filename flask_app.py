b6fa6ec41069041d66599142d2aa340454066456
Chloe008 <chloe.carter@mng.ch>
2026-01-10 12:50:54 +0000

    Update flask

--- flask_app.py
+++ flask_app.py
@@ -118,16 +118,17 @@ def logout():
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    # GET
- #  if request.method == "GET":
-  #      todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
-   #     return render_template("main_page.html", todos=todos)
+     # GET
+   if request.method == "GET":
+        todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
+        return render_template("main_page.html", todos=todos)
 
     # POST
-  #  content = request.form["contents"]
-   # due = request.form["due_at"]
-    #db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
-    #return redirect(url_for("index"))
+    content = request.form["contents"]
+    due = request.form["due_at"]
+    db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
+    return redirect(url_for("index"))
+
 
 @app.post("/complete")
 @login_required
