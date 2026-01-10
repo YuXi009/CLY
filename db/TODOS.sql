4fe83ec9ee87d7adb032cb7cb66eb24289a0c82d
Chloe008 <chloe.carter@mng.ch>
2026-01-10 13:00:40 +0000

    add user table to db

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,3 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL
+);
+
 CREATE TABLE Patient (
   patienten_id INT PRIMARY KEY,
   Name VARCHAR (100) NOT NULL,
